dfdce1e8dd3704e45e13614d900b8fb0ec7584b7
CSS-Lletya <51089431+CSS-Lletya@users.noreply.github.com>
2023-06-10 08:37:49 -0400

    Feature: Key Pressed packet
    The Key Pressed packet is found, added a feature to close interfaces with ESC key

--- network/com/rs/net/packets/outgoing/impl/KeyTypedPacket.java
+++ network/com/rs/net/packets/outgoing/impl/KeyTypedPacket.java
@@ -1,15 +1,23 @@
 package com.rs.net.packets.outgoing.impl;
 
+import com.rs.GameConstants;
 import com.rs.game.player.Player;
 import com.rs.io.InputStream;
 import com.rs.net.packets.outgoing.OutgoingPacketListener;
 import com.rs.net.packets.outgoing.OutgoingPacketSignature;
+import com.rs.utilities.LogUtility;
+import com.rs.utilities.LogUtility.LogType;
 
 @OutgoingPacketSignature(packetId = 63, description = "Represents a Player's Key input (can be used for ESC close interface, etc..)")
 public class KeyTypedPacket implements OutgoingPacketListener {
 
 	@Override
 	public void execute(Player player, InputStream stream) {
-		
+		int key = stream.readByte();
+		if (GameConstants.DEBUG)
+			LogUtility.log(LogType.DEBUG, "Key: " + key);
+		if (key == 13) {
+			player.getMovement().stopAll(false, true, false);
+		}
 	}
 }
