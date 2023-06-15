2eb945f887a9694828fac73c71f1973c85e1b656
CSS-Lletya <51089431+CSS-Lletya@users.noreply.github.com>
2023-06-15 19:06:16 -0400

    Feature: 24 Hour session kicking
    You will now be force kicked if you've been "playing" for over 24 hours. 10 minute warning message prior to kick

--- network/com/rs/net/packets/outgoing/impl/IdlePacket.java
+++ network/com/rs/net/packets/outgoing/impl/IdlePacket.java
@@ -1,5 +1,6 @@
 package com.rs.net.packets.outgoing.impl;
 
+import com.rs.GameConstants;
 import com.rs.game.player.Player;
 import com.rs.io.InputStream;
 import com.rs.net.packets.outgoing.OutgoingPacketListener;
@@ -13,5 +14,9 @@ public class IdlePacket implements OutgoingPacketListener {
 		int timer = stream.readShort();
 		if (timer % 18000 == 0)
 			player.getPackets().sendGameMessage("You have been playing for quite a while now, please consider taking a break!");
+		if (timer == 86040)
+			player.getPackets().sendGameMessage("You will be logged out in 10 minutes for playing "+GameConstants.SERVER_NAME+" for over 24 hours.");
+		if (timer == 86400)
+			player.getSession().realFinish(player, true);
 	}
 }
