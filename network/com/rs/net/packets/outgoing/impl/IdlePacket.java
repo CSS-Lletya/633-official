37e95611de539fe51b19e130aff1a46735c01677
CSS-Lletya <51089431+CSS-Lletya@users.noreply.github.com>
2023-06-15 19:01:53 -0400

    Feature: Every 5 Hours gameplay warning
    Every 5 hours a message will be sent to the player to consider taking a break.

--- network/com/rs/net/packets/outgoing/impl/IdlePacket.java
+++ network/com/rs/net/packets/outgoing/impl/IdlePacket.java
@@ -8,9 +8,10 @@ import com.rs.net.packets.outgoing.OutgoingPacketSignature;
 @OutgoingPacketSignature(packetId = 71, description = "Represents a Player's idle timer to the server")
 public class IdlePacket implements OutgoingPacketListener {
 
-	@SuppressWarnings("unused")
 	@Override
 	public void execute(Player player, InputStream stream) {
-		int unknown = stream.readShort();
+		int timer = stream.readShort();
+		if (timer % 18000 == 0)
+			player.getPackets().sendGameMessage("You have been playing for quite a while now, please consider taking a break!");
 	}
 }
