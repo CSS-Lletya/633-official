feaf235215a29d728b09cb17f32992a6d7721eea
CSS-Lletya <51089431+CSS-Lletya@users.noreply.github.com>
2023-06-24 23:37:22 -0400

    Refactor: Rework Item Weights class
    Item Weights class now reads unpacked file (easier to manage).
    
    Updated some weight reducing items according to the wiki:
    https://runescape.fandom.com/wiki/Weight-reducing_clothing?oldid=3567935

--- src/com/rs/game/item/ItemWeights.java
+++ src/com/rs/game/item/ItemWeights.java
@@ -1,38 +1,79 @@
 package com.rs.game.item;
 
-import java.io.RandomAccessFile;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
-import java.nio.channels.FileChannel.MapMode;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 
+import com.rs.utilities.LogUtility;
+import com.rs.utilities.LogUtility.LogType;
+
+import lombok.SneakyThrows;
+
+/**
+ * Item weights according to the standards of 2010 runescape. All available
+ * weight reductions has been added.
+ * 
+ * @author Dennis
+ *
+ */
 public class ItemWeights {
 
+	/**
+	 * The Item Weights map
+	 */
 	private final static HashMap<Integer, Double> itemWeights = new HashMap<Integer, Double>();
-	private final static String PACKED_PATH = "data/items/packedWeights.dat";
 
+	/**
+	 * The path to the item weights file
+	 */
+	private final static String UNPACKED_PATH = "data/items/weights.txt";
+
+	/**
+	 * Loads the Item weights data
+	 */
 	public static final void init() {
-		loadPackedItemWeights();
+		load();
 	}
 
-	public static final double getWeight(Item item, boolean equiped) {
+	/**
+	 * Gets the Item weight itself and checks if it's being equiped (for possible
+	 * reduction effect)
+	 * 
+	 * @param item
+	 * @param equiped
+	 * @return
+	 */
+	public static double getWeight(Item item, boolean equiped) {
 		Double weight = itemWeights.get(item.getId());
 		if (weight == null || item.getDefinitions().isNoted())
 			return 0;
 		return weight < 0.0 ? equiped ? weight : 0 : weight;
 	}
 
-	private static void loadPackedItemWeights() {
-		try {
-			RandomAccessFile in = new RandomAccessFile(PACKED_PATH, "r");
-			FileChannel channel = in.getChannel();
-			ByteBuffer buffer = channel.map(MapMode.READ_ONLY, 0, channel.size());
-			while (buffer.hasRemaining())
-				itemWeights.put(buffer.getShort() & 0xffff, buffer.getDouble());
-			channel.close();
-			in.close();
-		} catch (Throwable e) {
-			e.printStackTrace();
+	/**
+	 * Populates the Item weights map and loads the data.
+	 */
+	@SneakyThrows(IOException.class)
+	private static void load() {
+		LogUtility.log(LogType.INFO, "Loading Item weights.");
+		BufferedReader in = new BufferedReader(new FileReader(UNPACKED_PATH));
+		while (true) {
+			String line = in.readLine();
+			if (line == null)
+				break;
+			if (line.startsWith("//"))
+				continue;
+			line = line.replace("﻿", "");
+			String[] splitedLine = line.split(" - ", 2);
+			if (splitedLine.length < 2) {
+				in.close();
+				throw new RuntimeException("Invalid list for item weight line: " + line);
+			}
+			int itemId = Integer.valueOf(splitedLine[0]);
+			double weight = Double.valueOf(splitedLine[1]);
+			itemWeights.put(itemId, weight);
 		}
+		in.close();
 	}
 }
