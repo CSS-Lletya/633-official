fde5a7527ab5819d73ab49330ec6c77e4e586af7
CSS-Lletya <51089431+CSS-Lletya@users.noreply.github.com>
2023-07-23 13:31:02 -0400

    Feature: Wise Old Man
    You can now buy your quest cape (no quest points set in place so its free beyond the 99k payment)

---
+++ plugins/com/rs/plugin/impl/npcs/region/DraynorVillageRegionNPCPlugin.java
@@ -0,0 +1,50 @@
+package com.rs.plugin.impl.npcs.region;
+
+import com.rs.game.dialogue.DialogueEventListener;
+import com.rs.game.dialogue.Expression;
+import com.rs.game.npc.NPC;
+import com.rs.game.player.Inventory;
+import com.rs.game.player.Player;
+import com.rs.plugin.listener.NPCListener;
+import com.rs.plugin.wrapper.NPCSignature;
+
+@NPCSignature(name = {}, npcId = {3820})
+public class DraynorVillageRegionNPCPlugin extends NPCListener {
+
+	@Override
+	public void execute(Player player, NPC npc, int option) {
+		player.dialogue(new DialogueEventListener(player, npc) {
+			@Override
+			public void start() {
+				npc(Expression.question, "Greetings! What can I do for you?");
+				if (player.getDetails().getQuestPoints().get() == 0) {
+					player(Expression.happy, "I was wondering if you could sell me a quest cape!"," I have completed all the quests.");
+					npc(Expression.holy_shit, "Impressive! I see you have! It will ","cost you 99,000 coins, though.");
+					option("Yes, I have that with me now.", () -> {
+						if (player.getInventory().getFreeSlots() < 2) {
+							player.getPackets().sendGameMessage(Inventory.INVENTORY_FULL_MESSAGE);
+							this.complete();
+							return;
+						}
+						if (player.getInventory().canPay(99000)) {
+							event(() -> {
+								player.getInventory().addItemDrop(9814, 1);
+								player.getInventory().addItemDrop(9813, 1);
+							});
+							player.dialogue(npc.getId(), d -> {
+								d.player(Expression.happy_plain_eyebrows_up, "Yeah I have that with me. Here you go.");
+								d.npc(Expression.happy, "Wear the cape with pride, adventurer.");
+							});
+						} else {
+							npc(Expression.sad, "I'm afraid you dont have enough money to buy this.");
+						}
+					}, "Sorry, nevermind.", () -> this.complete());
+					
+				} else {
+					player(Expression.afraid, "I'm not sure. What can you do for me?");
+					npc(Expression.happy, "I can offer you a quest cape "," once you reach maximum quest points.");
+				}
+			}
+		});
+	}
+}
